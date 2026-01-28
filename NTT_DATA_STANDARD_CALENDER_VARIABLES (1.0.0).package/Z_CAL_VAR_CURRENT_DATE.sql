ca1c981cbc87aa30edb9c90707e3c0e9cd1a690f
Pratik vishe <110523015+Tacit12@users.noreply.github.com>
2026-01-28 12:38:03 +0530

    Add CURRENT_DATE selection in SQL query

---
+++ NTT_DATA_STANDARD_CALENDER_VARIABLES (1.0.0).package/Z_CAL_VAR_CURRENT_DATE.sql
@@ -0,0 +1,8 @@
+/*----------------------------------------------------------
+  Variable Name : Z_CAL_VAR_CURRENT_DATE
+  Purpose       : Returns current system date
+-----------------------------------------------------------*/
+SELECT
+    CURRENT_DATE AS CURRENT_DATE
+FROM DUMMY;
+
